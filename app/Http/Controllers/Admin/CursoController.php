9cb688dd8edec587be43c45dc39d34e2b0dd3536
Tiago Soares Brandao <ra2457033>
2026-05-09 09:30:35 -0300

    2 aula

--- app/Http/Controllers/Admin/CursoController.php
+++ app/Http/Controllers/Admin/CursoController.php
@@ -9,6 +9,53 @@ class CursoController extends Controller
 {
     public function index(){
         $rows = Curso::all();
+
         return view('admin.cursos.index', compact('rows'));
+    }
+
+    public function adicionar(){
+        return view('admin.cursos.adicionar');
+    }
+
+    public function salvar(Request $request){
+       $dados = $request->all();
+        
+        if($request->hasFile('arquivo')){
+            $imagem = $request->file('arquivo');
+            $num = rand(1111,9999);
+            $dir = "img/cursos/";
+            $ex = $imagem->guessClientExtension();
+            $nomeImagem = "imagem_".$num.".".$ex;
+            $imagem->move($dir,$nomeImagem);
+            $dados['imagem'] = $dir.$nomeImagem;
+        }
+        Curso::create($dados);
+        return redirect()->route('admin.cursos');
+    }
+
+    public function editar($id){
+        $row = Curso::find($id);
+        return view('admin.cursos.editar', compact('row'));
+    }
+
+    public function atualizar(Request $request, $id){
+        $dados = $request->all();
+        
+        if($request->hasFile('arquivo')){
+            $imagem = $request->file('arquivo');
+            $num = rand(1111,9999);
+            $dir = "img/cursos/";
+            $ex = $imagem->guessClientExtension();
+            $nomeImagem = "imagem_".$num.".".$ex;
+            $imagem->move($dir,$nomeImagem);
+            $dados['imagem'] = $dir.$nomeImagem;
+        }
+        Curso::find($id)->update($dados);
+        return redirect()->route('admin.cursos');
+    } 
+
+    public function excluir($id){
+        Curso::find($id)->delete();
+        return redirect()->route('admin.cursos');
     }
 }
